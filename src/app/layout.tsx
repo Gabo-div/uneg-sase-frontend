459f782fbb3ef88bd06d18c32179039d1db2d4d7
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-04-07 17:55:32 -0400

    feat(app): instalado momentjs y configurado para el idioma español

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import Providers from "@/components/Providers";
+
+import 'moment/locale/es';
+import moment from "moment";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+moment.locale("es");
 
 export const metadata: Metadata = {
   title: "SASE",
