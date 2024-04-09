b6415d8808b4625413e3fb85d579548f248f963d
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-04-08 20:46:51 -0400

    chore: prettier y plugin de prettier para tailwind

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: false,
+	quoteProps: "as-needed",
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	rangeStart: 0,
+	rangeEnd: Infinity,
+	endOfLine: "lf",
+	plugins: ["prettier-plugin-tailwindcss"],
+}
